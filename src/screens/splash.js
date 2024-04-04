cc001dd8b45e2410bbf2b4603db7afe3b94e2cbb
rizwanatta <rizwanatta@protonmail.com>
2024-04-04 10:45:46 +0500

    splash delay, and auto navigation on useEffect

--- src/screens/splash.js
+++ src/screens/splash.js
@@ -1,9 +1,26 @@
 import { View, Image } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import { getUserUid } from "../services/database-service";
 
-const Splash = () => {
+const Splash = ({ navigation }) => {
   //#TODO get userUID and based on that run app's flow
-  // if user uid is there go to home else go to login
+
+  // asa effect jo empty bracket k sath h wo one time on screen load chalta h!
+  useEffect(() => {
+    handleUserIdFLow();
+  }, []);
+
+  const handleUserIdFLow = async () => {
+    const id = await getUserUid();
+
+    setTimeout(() => {
+      if (id === "" || id === null) {
+        navigation.replace("Login");
+      } else {
+        navigation.replace("Home");
+      }
+    }, 3000);
+  };
 
   return (
     <View className={"flex flex-1 bg-red-200 justify-center items-center"}>
